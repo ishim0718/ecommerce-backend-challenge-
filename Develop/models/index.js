5528403d4c08c375fd196e4b03c1b37c273b731d
ishim0718 <ishim0718@gmail.com>
2023-04-25 21:25:43 -0400

    minor fix

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -16,7 +16,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongToMany(Tag, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
-Tag.belongToMany(Product { through: ProductTag });
+Tag.belongToMany(Product, { through: ProductTag });
 
 module.exports = {
   Product,
