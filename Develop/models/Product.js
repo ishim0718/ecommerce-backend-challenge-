1bb480055ec0ec216c2c194c207c1d40bd9221a4
ishim0718 <ishim0718@gmail.com>
2023-04-25 21:24:32 -0400

    comepleted relational mapping

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -34,8 +34,14 @@ Product.init(
       validate: {
         isNumeric: true
       }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
     }
-
   },
   {
     sequelize,
